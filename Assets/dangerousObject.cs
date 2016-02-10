e226a8a9000a38fceb8efcec620ae6453c5a6ba1
Alexander Young <Alexander Young>
2016-02-10 00:56:02 -0500

    added slide jump and fixed a few things

---
+++ Assets/dangerousObject.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class dangerousObject : MonoBehaviour {
+
+	public Player player;
+	
+	void OnTriggerEnter(Collider col){
+		print("ya hit me");
+		if(col.CompareTag("Player")){
+			player.defeated();
+		}
+	}
+}
