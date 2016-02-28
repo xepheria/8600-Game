6b93db8f8070573512bc8e2db906a03b1eb2c65c
Alexander Young <Alexander Young>
2016-02-28 16:40:09 -0500

    some new prefabs and organization

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -396,8 +396,6 @@ public class Player : MonoBehaviour {
 			//not pressing anything, gravity kicks in
 			else if(controller.collisions.below){
 				xsp = Mathf.Lerp(xsp, xsp-(Mathf.Min(Mathf.Abs(xsp), frc)*Mathf.Sign(xsp)), Time.deltaTime);
-				//if(Mathf.Abs(xsp) < 0.005f)
-				//	xsp = 0;
 			}
 			else if(ysp > 0 && ysp < 1){	//air drag
 				if (Mathf.Abs(xsp) > 0.05f)
@@ -423,7 +421,7 @@ public class Player : MonoBehaviour {
 			}
 			
 			//if we let go of jump button early
-			if(Input.GetButtonUp("Jump") && ysp > jmp*.3f){
+			if(Input.GetButtonUp("Jump") && ysp > jmp*.3f && ysp < jmp){
 				ysp = jmp*.3f;
 			}
 			
@@ -437,6 +435,14 @@ public class Player : MonoBehaviour {
 			//cap to max speed
 			if(controller.collisions.below && launchTimer <= 0){
 				//xsp = Mathf.Clamp(xsp, -top, top);
+				if(xsp > top){
+					xsp = Mathf.Lerp(xsp, top, Time.deltaTime*aboveTopDec);
+				}
+				if(xsp < -top){
+					xsp = Mathf.Lerp(xsp, -top, Time.deltaTime*aboveTopDec);	
+				}
+			}
+			else if(!controller.collisions.below){
 				if(xsp > top){
 					xsp = Mathf.Lerp(xsp, top, Time.deltaTime*aboveTopDec);
 				}
---
+++ Assets/Scripts/boostRingBehavior.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent (typeof(BoxCollider))]
+[RequireComponent (typeof(Rigidbody))]
+[RequireComponent (typeof(Animator))]
+public class boostRingBehavior : MonoBehaviour {
+
+	public float boostAmt;
+	public Player player;
+	private float y; //used for floating effect
+	private float amplitude = 0.06f;
+	private float speed = 1f;
+	
+	public Animator anim;
+
+	// Use this for initialization
+	void Start () {
+		player = FindObjectOfType<Player>();
+		y = transform.position.y;
+		GetComponent<Animator>();
+	}
+	
+	void OnTriggerEnter(Collider col){
+		print("collision entered");
+		if(col.CompareTag("Player")){
+			print("boost!");
+			player.bounce(transform.up * boostAmt);
+			anim.Play("Boost");
+		}
+	}
+	
+	void Update(){
+		transform.position = new Vector3(transform.position.x, y + amplitude * Mathf.Sin(speed*Time.time), 0);
+	}
+}
