8236eb2494da59c2279e98a567c9c59840e0a063
Wynton Redmond <wyntonr@m0005.fx.clemson.edu>
2016-04-19 20:06:47 -0400

    see prev, also quality settings

--- Assets/Scripts/animatedSequenceScript.cs
+++ Assets/Scripts/animatedSequenceScript.cs
@@ -47,7 +47,7 @@ public class animatedSequenceScript : MonoBehaviour {
 
 	IEnumerator PlayLoop(float delay){
 		yield return new WaitForSeconds (delay);
-		frameCounter += 2;	
+		frameCounter += 3;	
 		StopCoroutine ("PlayLoop");
 	}
 		
